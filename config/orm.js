980767b6cb5f62a1b7fd9b17edc993e2283dc3aa
ducktrshessami <jwee761@gmail.com>
2021-01-08 15:15:20 -0800

    promisified query helper function

--- config/orm.js
+++ config/orm.js
@@ -10,6 +10,16 @@ updateOne
 
 const connection = require("./connection");
 
+// Promisified connection.query
+function query(query, values) {
+    return new Promise((resolve, reject) => {
+        connection.query(query, values, (err, res) => {
+            if (err) reject(err);
+            else resolve(res);
+        });
+    });
+}
+
 module.exports = {
     selectAll: function() {
 
