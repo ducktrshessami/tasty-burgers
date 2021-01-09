eb12a2e4d11c3e99253dc3674cd7753034f63fda
ducktrshessami <jwee761@gmail.com>
2021-01-09 10:13:49 -0800

    update sql files to be unspecific to a database

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,3 @@
-drop database if exists burgers_db;
-create database burgers_db;
-use burgers_db;
-
 create table burgers(
     id int primary key auto_increment not null,
     burger_name varchar(100) not null,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
-use burgers_db;
-
 insert into burgers(burger_name, devoured) values
 ("Hamburger", false),
 ("Cheeseburger", true),
