1d9c912643a0a101c8ca7087ca9d140715a57bce
ducktrshessami <jwee761@gmail.com>
2021-01-09 11:19:41 -0800

    commented database creation queries

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
+-- drop database if exists burgers_db;
+-- create database burgers_db;
+-- use burgers_db;
+
 create table burgers(
     id int primary key auto_increment not null,
     burger_name varchar(100) not null,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
+-- use burgers_db;
+
 insert into burgers(burger_name, devoured) values
 ("Hamburger", false),
 ("Cheeseburger", true),
